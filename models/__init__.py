f26c68bdb5d2274862831a83c8500318a4ebabd7
tanyawty <164116746+tanyawty@users.noreply.github.com>
2026-01-29 09:44:35 +0000

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,9 @@
-from .mech_aware_stgnn import (
-    LearnedGraphAttn,
-    GraphConv,
-    MechAware_GP_STGNN_MultiTask,
-    seq_to_pf_ma_gap,
-)
+from .stgnn_learn import STGNN
+
+__all__ = [
+    "LearnedGraphAttn",
+    "GraphConv",
+    "MechAware_GP_STGNN_MultiTask",
+    "seq_to_pf_ma_gap",
+    "STGNN_LearnOnly",
+]
