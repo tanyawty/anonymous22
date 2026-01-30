8451f8f169757851b9f510fa974e677998310a64
tanyawty <164116746+tanyawty@users.noreply.github.com>
2026-01-30 00:24:46 +0000

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,13 +1,13 @@
 # models/__init__.py
 
-from .stgnn import STGNN
+from .stgnn import STGNN_LearnOnly
 from .fouriergnn import FourierGNN_LearnOnly
 from .patchtst import PatchTST_Baseline
 from .classical_baselines import GRU_Baseline, LSTM_Baseline, TCN_Baseline, Transformer_Baseline, MLP_Baseline
 from .gp_mech_stgnn import GPMechSTGNN
 
 __all__ = [
-    "STGNN",
+    "STGNN_LearnOnly",
     "FourierGNN_LearnOnly",
     "PatchTST_Baseline",
     "GRU_Baseline",
